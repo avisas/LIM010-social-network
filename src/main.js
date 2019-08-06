8d45268ffa4e1e7e34f029a32a08591a784dc883
ayauri29 <albayauri.29@gmail.com>
2019-08-06 09:09:26 -0500

    Agregando firebase al proyecto y creación de la función userCreate close #4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+/*import { myFunction } from './lib/index.js';
 
-myFunction();
+myFunction();*/
+
+
+
+let formAutenticacion;
+inicializar = () => {
+  formAutenticacion = document.getElementById("form-autenticacion");
+  formAutenticacion.addEventListener("submit", userCreate, false);
+}
+
+userCreate = (event) => {
+  event.preventDefault();
+  var usuario = event.target.email.value;
+  var contrasena = event.target.password.value;
+
+  firebase.auth().createUserWithEmailAndPassword(usuario, contrasena)
+    .then(function (result) {
+      console.log('Usuario creado');
+    })
+    .catch(function (error) {
+      console.log('Error');
+    });
+}
+inicializar();
