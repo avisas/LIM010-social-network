fe8351daa36fe3960c764ff3ff6f02285e7830be
Jesseliz <choque101108@gmail.com>
2019-08-14 09:18:02 -0500

    icon eye for show password content

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -1,5 +1,8 @@
-import { recoverUserName, pushPublication, pullAllPublications, changeViewToProfile, signOutUser} from '../controller/home-controller.js';
-import {savePost, showPost} from '../controller/post-controller.js';
+import {
+ recoverUserName, pushPublication, pullAllPublications, changeViewToProfile, signOutUser 
+} from '../controller/home-controller.js';
+import { savePost, showPost } from '../controller/post-controller.js';
+
 export const home = () => {
   const home = document.createElement('div');
 
@@ -42,46 +45,20 @@ export const home = () => {
   home.innerHTML = homeContent;
 
   const userName = home.querySelector('#user-name');
-  const formPublication = home.querySelector('#form-publication');
   const allPublications = home.querySelector('#listOfPublications');
 
   const btnSignOut = home.querySelector('#signOut');
   // const notePost = home.querySelector('#publication').value;
   const btnComportirPost = home.querySelector('#compartir-post');
-  
-  //Delete
-  
 
   btnSignOut.addEventListener('click', signOutUser);
   recoverUserName(userName);
 
-  formPublication.addEventListener('submit', () => {
-    console.log('Se ejecutó submit');
-    pushPublication(event);
-    renderAllPublications(allPublications, pullAllPublications());
-  });
 
   userName.addEventListener('click', changeViewToProfile);
 
-  renderAllPublications(allPublications, pullAllPublications());
   btnComportirPost.addEventListener('click', savePost);
 
   showPost(allPublications);
   return home;
 };
-
-export const renderAllPublications = (element, list) => {
-  console.log('Se ejecutó el render');
-  element.innerHTML= '';
-  for (let publication of list) {
-    let listItem = document.createElement('li');
-    listItem.innerHTML = `
-      Publicado por ${publication.displayName}. El mensaje es: ${publication.message}
-      <a id="L${publication.id}">Like</a>
-      <a id="E${publication.id}">Edit</a>
-    `;
-    element.appendChild(listItem);
-  }
-};
-
-
