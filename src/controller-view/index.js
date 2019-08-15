80780d1a19bba94598641ac039ef90fd842bdbfb
Jesseliz <choque101108@gmail.com>
2019-08-15 13:05:37 -0500

    agrenado validaciones de slint

--- src/controller-view/index.js
+++ src/controller-view/index.js
@@ -1,32 +1,20 @@
+/* eslint-disable import/no-cycle */
 import { components } from '../views/index.js';
 
 export const changeView = (route) => {
   const container = document.getElementById('container');
-// const sessionUser = firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
-// FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+  // const sessionUser = firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+  // FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
   container.innerHTML = '';
   switch (route) {
-    case '#/':
-      {
-        container.appendChild(components.login());
-      }
+    case '#/': container.appendChild(components.login());
       break;
-    case '#/register':
-      {
-        container.appendChild(components.register());
-      }
+    case '#/register': container.appendChild(components.register());
       break;
-      
-    case '#/home':
-      {
-        container.appendChild(components.home());
-      }
+    case '#/home': container.appendChild(components.home());
       break;
-    case '#/profile':
-      {
-        container.appendChild(components.profile());
-      }
+    case '#/profile': container.appendChild(components.profile());
       break;
     default: break;
   }
