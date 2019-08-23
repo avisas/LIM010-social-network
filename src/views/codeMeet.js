daae79a78b65b10f0f689f9f281a034c9f39d934
ayauri29 <albayauri.29@gmail.com>
2019-08-23 10:29:55 -0500

    Poniendo nombre a lado de la foto de perfil

--- src/views/codeMeet.js
+++ src/views/codeMeet.js
@@ -26,7 +26,7 @@ export default () => {
         <img class="img-profile" src="img/banner03.jpg">
         <div>
           ${userCurrent().photoURL !== null ? `<img src="${userCurrent().photoURL}">` : '<img src="">'}
-          <h2>
+          <h2>${userCurrent().displayName}</h2>
         </div>
       </div>
       <div id="content-post">
