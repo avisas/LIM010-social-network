e28ce5eb627711a64c083042a085d9000ec48088
Jesseliz <choque101108@gmail.com>
2019-08-26 07:51:13 -0500

    user name and photo

--- src/controller/home-controller.js
+++ src/controller/home-controller.js
@@ -1,18 +1,21 @@
+/* eslint-disable brace-style */
 import { signOutLogin } from '../controller-firebase/controller-authentication.js';
 
-export const recoverUserName = (userName, userProfileName, UserPhoto) => {
+export const recoverUserName = (userName, userProfileName, UserPhotoProfile) => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       const displayName = `<span class="icon-user"></span>${user.displayName}`;
       const displayUserName = user.displayName;
       const userPhoto = user.photoURL;
-      const userEmail = user.email;
-      if (displayName === null) {
-        userName.textContent = userEmail;
-      } else {
+      if (userPhoto === null) {
         userName.innerHTML = displayName;
         userProfileName.innerHTML = displayUserName;
-        UserPhoto.src = userPhoto;
+        UserPhotoProfile.src = 'https://icon-library.net/images/avatar-icon-png/avatar-icon-png-16.jpg';
+      }
+      else {
+        userName.innerHTML = displayName;
+        userProfileName.innerHTML = displayUserName;
+        UserPhotoProfile.src = userPhoto;
       }
     }
   });
@@ -29,7 +32,7 @@ export const changeViewToProfile = () => {
   window.location.hash = '#/profile';
 };
 export const changeViewToMyPosts = () => {
- document.location.hash = '#/myPost';
+  document.location.hash = '#/myPost';
 };
 export const changeViewToHome = () => {
   window.location.hash = '#/home';
