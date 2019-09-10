fd7fa74a3c76ff9929848bc199e808350f96d9eb
Jesseliz <choque101108@gmail.com>
2019-09-10 11:11:00 -0500

    observadores

--- src/views/index.js
+++ src/views/index.js
@@ -2,7 +2,7 @@ import register from './register-view.js';
 import login from './login-view.js';
 import { home } from './home-view.js';
 import profile from './profile-view.js';
-import codeMeet from './codeMeet.js';
+import { codeMeet } from './codeMeet.js';
 import { myPost } from './myPost-view.js';
 
 export const components = {
